1341174b49a031758f20009baeed191ffef78c08
ryuuzenvs <dhankaede@gmail.com>
2026-01-09 10:16:20 +0700

    0.1.1

---
+++ app/Http/Middleware/RoleManager.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleManager
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+
+get roole param,
+if auth cek , auth usr role != role
+abrt 403
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        if (Auth::check() | Auth::user->role !== $role){
+        abort(403, '')
+    }
+        return $next($request);
+    }
+}
